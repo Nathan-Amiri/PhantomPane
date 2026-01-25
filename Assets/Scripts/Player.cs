58b7eac7b31bf4120adc599d3d084789acdd9b25
Nathan-Amiri <convenientlyoblivious@gmail.com>
2026-01-25 17:23:07 -0600

    Removed Dash, Added Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,12 @@ public class Player : MonoBehaviour
 
     // CONSTANT:
     private readonly float moveSpeed = 8;
-    private readonly float dashForce = 15;
+    private readonly float jumpForce = 15.5f;
+    //private readonly float dashForce = 15;
     private readonly float fallMultiplier = 3; // Fastfall
-    private readonly float diagonalDashMultiplier = 2;
-    private readonly float horizontalDashMultiplier = 4.5f;
-    private readonly float verticalDashMultiplier = 1.05f;
+    //private readonly float diagonalDashMultiplier = 2;
+    //private readonly float horizontalDashMultiplier = 4.5f;
+    //private readonly float verticalDashMultiplier = 1.05f;
 
     // DYNAMIC:
     private bool isGrounded;
@@ -22,14 +23,16 @@ public class Player : MonoBehaviour
     private bool isStunned;
 
     private Vector2 moveInput;
-    private bool dashInput;
-    private bool hasDash;
+    private bool jumpInput;
+    private bool hasJump;
+    //private bool dashInput;
+    //private bool hasDash;
 
     private void Update()
     {
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (Input.GetButtonDown("Dash"))
-            dashInput = true;
+        if (Input.GetButtonDown("Jump"))
+            jumpInput = true;
 
         Vector3 tempMousePosition = Input.mousePosition;
         tempMousePosition.z = -Camera.main.transform.position.z;
@@ -56,35 +59,46 @@ public class Player : MonoBehaviour
             rb.linearVelocity += (fallMultiplier - 1) * Physics2D.gravity.y * Time.deltaTime * Vector2.up;
 
         if (isGrounded)
-            hasDash = true;
+            hasJump = true;
 
-        if (dashInput)
+        if (jumpInput)
         {
-            dashInput = false;
+            jumpInput = false;
 
-            if (hasDash)
+            if (hasJump)
             {
-                hasDash = false;
-
-                rb.linearVelocity = Vector2.zero;
-
-
-
-                // Because of the movement code, gravity, and the nature of diagonal vs straight dashing, different directions need custom values
-                Vector2 velocity = moveInput * dashForce;
-
-                if (moveInput.x != 0 && moveInput.y != 0) // Diagonal dashes get a horizontal boost
-                    velocity.x *= diagonalDashMultiplier;
-                else if (moveInput.x == 0) // Vertical dashes get a slight boost
-                    velocity *= verticalDashMultiplier;
-                else if (moveInput.y == 0) // Horizontal dashes get a large boost
-                    velocity *= horizontalDashMultiplier;
-
-
-
-                    rb.AddForce(velocity, ForceMode2D.Impulse);
+                hasJump = false;
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
         }
+
+        //if (dashInput)
+        //{
+        //    dashInput = false;
+
+        //    if (hasDash)
+        //    {
+        //        hasDash = false;
+
+        //        rb.linearVelocity = Vector2.zero;
+
+
+
+        //        // Because of the movement code, gravity, and the nature of diagonal vs straight dashing, different directions need custom values
+        //        Vector2 velocity = moveInput * dashForce;
+
+        //        if (moveInput.x != 0 && moveInput.y != 0) // Diagonal dashes get a horizontal boost
+        //            velocity.x *= diagonalDashMultiplier;
+        //        else if (moveInput.x == 0) // Vertical dashes get a slight boost
+        //            velocity *= verticalDashMultiplier;
+        //        else if (moveInput.y == 0) // Horizontal dashes get a large boost
+        //            velocity *= horizontalDashMultiplier;
+
+
+
+        //        rb.AddForce(velocity, ForceMode2D.Impulse);
+        //    }
+        //}
     }
 
 
